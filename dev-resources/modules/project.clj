967a8b6806719c4e5d8e59210526b4206ce6b956
aroemers <github@company.romeinszoon.nl>
2013-08-13 11:31:07 +0200

    Initial commit.

---
+++ dev-resources/modules/project.clj
@@ -0,0 +1,8 @@
+(defproject module "1.0"
+  :dependencies [;; [org.clojure/clojure "1.5.1"]
+                 [org.clojars.tcrawley/clojure "1.6.0-clearthreadlocals"]
+                 ]
+  :exclusions [org.clojure/clojure]
+  :boxure {:start module.core/start
+           :stop module.core/stop
+           :config {:module/connect "localhost:123"}})
