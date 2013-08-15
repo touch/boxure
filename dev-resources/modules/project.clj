1b746bf1ec230ccda56e14049dfe8e3078eeda6d
aroemers <github@company.romeinszoon.nl>
2013-08-16 00:05:44 +0200

    Use Thread for cleaning, no patched Clojure needed.

--- dev-resources/modules/project.clj
+++ dev-resources/modules/project.clj
@@ -1,6 +1,6 @@
 (defproject module "1.0"
-  :dependencies [;; [org.clojure/clojure "1.5.1"]
-                 [org.clojars.tcrawley/clojure "1.6.0-clearthreadlocals"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 ;; [org.clojars.tcrawley/clojure "1.6.0-clearthreadlocals"]
                  ]
   :exclusions [org.clojure/clojure]
   :boxure {:start module.core/start
