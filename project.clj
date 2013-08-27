4253d2b670e0e0c39ecdd2128834d3a6ffaa26b3
aroemers <github@company.romeinszoon.nl>
2013-08-27 12:43:05 +0200

    Use our patched Clojure.

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[nl.onlinetouch/clojure "1.5.1"]
                  [leiningen-core "2.2.0"]
                  [classlojure "0.6.6"]]
+  :excludes [[org.clojure/clojure]]
   :jvm-opts ["-XX:+UseConcMarkSweepGC"
              "-XX:+CMSClassUnloadingEnabled"
              "-XX:MaxPermSize=35m"
